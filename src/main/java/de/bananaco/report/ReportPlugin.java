6ee63fc4ec145b6f09a3e0360c99d6669b728ea8
keough99 <DabblecraftInc@outlook.com>
2018-04-01 20:11:16 -0400

    update
    Co-Authored-By: Adam <afranklint16@gmail.com>

--- src/main/java/de/bananaco/report/ReportPlugin.java
+++ src/main/java/de/bananaco/report/ReportPlugin.java
@@ -1,14 +1,10 @@
 package de.bananaco.report;
 
-import de.bananaco.report.commands.Commands;
-import de.bananaco.report.listeners.ModChat;
-import de.bananaco.report.listeners.ReportListener;
-import de.bananaco.report.msg.MessageManager;
-import de.bananaco.report.report.ReportManager;
 import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Level;
+
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -16,96 +12,108 @@ import org.bukkit.permissions.Permission;
 import org.bukkit.permissions.PermissionDefault;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import de.bananaco.report.commands.Commands;
+import de.bananaco.report.listeners.GUIListener;
+import de.bananaco.report.listeners.ModChat;
+import de.bananaco.report.listeners.ReportListener;
+import de.bananaco.report.msg.MessageManager;
+import de.bananaco.report.report.ReportManager;
+
 public class ReportPlugin extends JavaPlugin {
 
-    private ReportManager rm;
-    private Config config;
-    private ReportListener listener;
-    private ModChat modchat;
-    private Commands command;
-    private MessageManager msgManager;
+	private ReportManager rm;
+	private Config config;
+	private ReportListener listener;
+	private ModChat modchat;
+	private Commands command;
+	private MessageManager msgManager;
+	private GUIListener gui;
 
-    public ReportPlugin() {
-        this.msgManager = new MessageManager(this);
-        this.rm = new ReportManager(this);
-        this.config = new Config(this);
-        this.listener = new ReportListener(this);
-        this.modchat = new ModChat();
-        this.command = new Commands(this);
-    }
+	public ReportPlugin() {
+		msgManager = new MessageManager(this);
+		rm = new ReportManager(this);
+		config = new Config(this);
+		listener = new ReportListener(this);
+		modchat = new ModChat();
+		command = new Commands(this);
+		gui = new GUIListener(this);
+	}
 
-    @Override
-    public void onDisable() {
-        rm.save();
-        log("Disabled");
-    }
+	@Override
+	public void onDisable() {
+		rm.save();
+		log("Disabled");
+	}
 
-    @Override
-    public void onEnable() {
-        getServer().getPluginManager().registerEvents(listener, this);
-        getServer().getPluginManager().registerEvents(modchat, this);
-        getCommand("report").setExecutor(command);
-        getCommand("read").setExecutor(command);
-        getCommand("gotoreport").setExecutor(command);
-        getCommand("resolve").setExecutor(command);
-        getCommand("unresolve").setExecutor(command);
-        getCommand("modchat").setExecutor(command);
-        getCommand("comment").setExecutor(command);
-        registerPermissions();
-        msgManager.load();
-        rm.load();
-        config.load();
-        log("Enabled");
-    }
+	@Override
+	public void onEnable() {
+		getServer().getPluginManager().registerEvents(listener, this);
+		getServer().getPluginManager().registerEvents(modchat, this);
+		getServer().getPluginManager().registerEvents(gui, this);
+		getCommand("report").setExecutor(command);
+		getCommand("read").setExecutor(command);
+		getCommand("gotoreport").setExecutor(command);
+		getCommand("resolve").setExecutor(command);
+		getCommand("unresolve").setExecutor(command);
+		getCommand("modchat").setExecutor(command);
+		getCommand("comment").setExecutor(command);
+		getCommand("rgui").setExecutor(command);
+		registerPermissions();
+		msgManager.load();
+		rm.load();
+		config.load();
+		log("Enabled");
+	}
 
-    public void log(String message) {
-        String newMessage = ChatColor.stripColor(message);
-        this.getLogger().log(Level.INFO, "{0}", newMessage);
-    }
+	public void log(String message) {
+		String newMessage = ChatColor.stripColor(message);
+		getLogger().log(Level.INFO, "{0}", newMessage);
+	}
 
-    public void log(String message, Level level) {
-        String newMessage = ChatColor.stripColor(message);
-        this.getLogger().log(level, "{0}", newMessage);
-    }
+	public void log(String message, Level level) {
+		String newMessage = ChatColor.stripColor(message);
+		getLogger().log(level, "{0}", newMessage);
+	}
 
-    public void registerPermissions() {
-        Map<String, Boolean> children = new HashMap<String, Boolean>();
-        // Add all the permission nodes we'll be using
-        children.put("breport.report", true);
-        children.put("breport.read", true);
-        children.put("breport.gotoreport", true);
-        children.put("breport.resolve", true);
-        children.put("breport.unresolve", true);
-        children.put("breport.modchat", true);
-        children.put("breport.comment", true);
-        // Put them under a parent
-        Permission perm = new Permission("breport.*", PermissionDefault.OP, children);
-        getServer().getPluginManager().addPermission(perm);
-    }
+	public void registerPermissions() {
+		Map<String, Boolean> children = new HashMap<String, Boolean>();
+		// Add all the permission nodes we'll be using
+		children.put("breport.report", true);
+		children.put("breport.read", true);
+		children.put("breport.gotoreport", true);
+		children.put("breport.resolve", true);
+		children.put("breport.unresolve", true);
+		children.put("breport.modchat", true);
+		children.put("breport.comment", true);
+		children.put("breport.gui", true);
+		// Put them under a parent
+		Permission perm = new Permission("breport.*", PermissionDefault.OP, children);
+		getServer().getPluginManager().addPermission(perm);
+	}
 
-    public String getName(CommandSender sender) {
-        String name;
-        if (!(sender instanceof Player)) {
-            name = "CONSOLE";
-        } else {
-            name = sender.getName();
-        }
-        return name;
-    }
+	public String getName(CommandSender sender) {
+		String name;
+		if (!(sender instanceof Player)) {
+			name = "CONSOLE";
+		} else {
+			name = sender.getName();
+		}
+		return name;
+	}
 
-    public ReportManager getReportManager() {
-        return this.rm;
-    }
+	public ReportManager getReportManager() {
+		return rm;
+	}
 
-    public Config getConf() {
-        return this.config;
-    }
+	public Config getConf() {
+		return config;
+	}
 
-    public MessageManager getMsgManager() {
-        return this.msgManager;
-    }
+	public MessageManager getMsgManager() {
+		return msgManager;
+	}
 
-    public File getDirectory(String Directory_Name) {
-        return new File(getDataFolder(), Directory_Name);
-    }
+	public File getDirectory(String Directory_Name) {
+		return new File(getDataFolder(), Directory_Name);
+	}
 }
--- src/main/java/de/bananaco/report/commands/Commands.java
+++ src/main/java/de/bananaco/report/commands/Commands.java
@@ -13,6 +13,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import de.bananaco.report.Config;
+import de.bananaco.report.GUI;
 import de.bananaco.report.ReportPlugin;
 import de.bananaco.report.msg.MessageManager;
 import de.bananaco.report.msg.Msg;
@@ -25,19 +26,20 @@ import de.bananaco.report.report.SendMail;
  * @author Brandon Barker
  */
 public class Commands implements CommandExecutor {
-	
+
 	private ReportPlugin rp;
 	private ReportManager rm;
 	private Config config;
 	private MessageManager mm;
-	
+
 	public Commands(ReportPlugin rp) {
 		this.rp = rp;
 		rm = rp.getReportManager();
 		config = rp.getConf();
 		mm = rp.getMsgManager();
 	}
-	
+
+	@Override
 	@SuppressWarnings("deprecation")
 
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -46,7 +48,8 @@ public class Commands implements CommandExecutor {
 		// Permission check for players
 		if (sender instanceof Player) {
 			if (!sender.hasPermission("breport." + cname)) {
-				// sender.sendMessage(ChatColor.RED + "[bR] You don't have permission to use the '/" + cname + "'
+				// sender.sendMessage(ChatColor.RED + "[bR] You don't have permission to use the
+				// '/" + cname + "'
 				// command.");
 				mm.msg(sender, Msg.PERMISSION, "'/" + cname + "' command.");
 				return true;
@@ -60,7 +63,8 @@ public class Commands implements CommandExecutor {
 				String[] report = args;
 				// How many words?
 				if (report.length < config.getReportLength()) {
-					// sender.sendMessage(ChatColor.RED + "[bR] Reports must be at least 3 words long.");
+					// sender.sendMessage(ChatColor.RED + "[bR] Reports must be at least 3 words
+					// long.");
 					mm.msg(sender, Msg.REPORT_LENGTH, config.getReportLength());
 					return true;
 				}
@@ -78,27 +82,30 @@ public class Commands implements CommandExecutor {
 				}
 				// And shout at them if there's too many similar reports
 				if (num > 0) {
-					// sender.sendMessage(ChatColor.RED + "[bR] You already have a ticket about that.");
+					// sender.sendMessage(ChatColor.RED + "[bR] You already have a ticket about
+					// that.");
 					mm.msg(sender, Msg.TICKET_CLONE);
 					return true;
 				}
 				// And file the report
 				Report r = rm.createReport(reporter, report, ((Player) sender).getLocation());
-				// sender.sendMessage(ChatColor.AQUA + "[bR] " + ChatColor.GRAY + "A report has been filed for you - ID:
+				// sender.sendMessage(ChatColor.AQUA + "[bR] " + ChatColor.GRAY + "A report has
+				// been filed for you - ID:
 				// " + ChatColor.AQUA + r.getID());
 				mm.msg(sender, Msg.REPORT_FILED_YOU, r.getID());
 				if (config.useMail()) {
 					String to = config.getMailTo();
 					String host = config.getHost();
 					String from = "noreply@" + Bukkit.getServer().getIp();
-					SendMail.mail(host, to, from, "New Ticket!",
-							r.getReporter() + " has filed ticket #" + r.getID() + ": " + r.getReport());
+					SendMail.mail(host, to, from, "New Ticket!", r.getReporter() + " has filed ticket #" + r.getID() + ": " + r.getReport());
 				}
 				// Inform all online admins
 				for (Player player : rp.getServer().getOnlinePlayers()) {
 					if (player.hasPermission("breport.read")) {
-						// player.sendMessage(ChatColor.AQUA + "[bR] " + ChatColor.GRAY + "A new report has been filed
-						// by " + ChatColor.AQUA + reporter + ChatColor.GRAY + " - ID: " + ChatColor.AQUA + r.getID());
+						// player.sendMessage(ChatColor.AQUA + "[bR] " + ChatColor.GRAY + "A new report
+						// has been filed
+						// by " + ChatColor.AQUA + reporter + ChatColor.GRAY + " - ID: " +
+						// ChatColor.AQUA + r.getID());
 						if (!((Player) sender).getName().equals(player.getName())) {
 							mm.msg(sender, Msg.REPORT_FILED, reporter, r.getID());
 						}
@@ -108,7 +115,8 @@ public class Commands implements CommandExecutor {
 				rp.log(mm.getMsg(Msg.REPORT_FILED, reporter, r.getID()));
 				return true;
 			} else {
-				// sender.sendMessage(ChatColor.RED + "[bR] Only players can use the '/report' command!");
+				// sender.sendMessage(ChatColor.RED + "[bR] Only players can use the '/report'
+				// command!");
 				mm.msg(sender, Msg.INGAMECOMMAND, "/report");
 				return true;
 			}
@@ -119,18 +127,20 @@ public class Commands implements CommandExecutor {
 				// How many shown?
 				int shown = 0;
 				int show = reports.size() - 1;
-				if (show > config.getTicketDisplay() - 1) {
+				if (show > (config.getTicketDisplay() - 1)) {
 					show = config.getTicketDisplay() - 1;
 				}
 				for (int i = show; i >= 0; i--) {
 					Report r = reports.get(i);
 					// Build the String
-					// prints.add(ChatColor.AQUA + r.getReporter() + ChatColor.GRAY + " - ID: " + ChatColor.AQUA +
+					// prints.add(ChatColor.AQUA + r.getReporter() + ChatColor.GRAY + " - ID: " +
+					// ChatColor.AQUA +
 					// r.getID());
 					prints.add(mm.getMsg(Msg.REPORT_READ, r.getReporter(), r.getID()));
 					shown++;
 				}
-				// sender.sendMessage(ChatColor.AQUA + "[bR] " + ChatColor.GRAY + "Showing " + ChatColor.AQUA + shown +
+				// sender.sendMessage(ChatColor.AQUA + "[bR] " + ChatColor.GRAY + "Showing " +
+				// ChatColor.AQUA + shown +
 				// "/" + reports.size() + ChatColor.GRAY + " unread reports");
 				mm.msg(sender, Msg.SHOWING_REPORT, shown, reports.size());
 				// New line for each report
@@ -146,17 +156,21 @@ public class Commands implements CommandExecutor {
 			} else {
 				String id = args[0];
 				if (rm.getReport(id) == null) {
-					// sender.sendMessage(ChatColor.RED + "[bR] No report with that id, use '/read' to see all
+					// sender.sendMessage(ChatColor.RED + "[bR] No report with that id, use '/read'
+					// to see all
 					// unresolved reports.");
 					mm.msg(sender, Msg.UNSOLVED_REPORTS, "read");
 					return true;
 				} else {
 					Report report = rm.getReport(id);
 					// Send the report data
-					// sender.sendMessage(ChatColor.AQUA + "[bR] " + ChatColor.GRAY + "Reporter: " + ChatColor.AQUA +
-					// report.getReporter() + ChatColor.GRAY + " ID: " + ChatColor.AQUA + report.getID());
+					// sender.sendMessage(ChatColor.AQUA + "[bR] " + ChatColor.GRAY + "Reporter: " +
+					// ChatColor.AQUA +
+					// report.getReporter() + ChatColor.GRAY + " ID: " + ChatColor.AQUA +
+					// report.getID());
 					mm.msg(sender, Msg.READ_REPORTER, report.getReporter(), report.getID());
-					// sender.sendMessage(ChatColor.AQUA + "[bR] " + ChatColor.GRAY + "Status: " + ChatColor.AQUA +
+					// sender.sendMessage(ChatColor.AQUA + "[bR] " + ChatColor.GRAY + "Status: " +
+					// ChatColor.AQUA +
 					// (report.getResolved() ? "resolved" : "open"));
 					mm.msg(sender, Msg.READ_STATUS, report.getResolved() ? "resolved" : "open");
 					// sender.sendMessage(ChatColor.GRAY + "** " + report.getReport());
@@ -164,10 +178,11 @@ public class Commands implements CommandExecutor {
 					return true;
 				}
 			}
-		} else if (cname.equals("resolve") && args.length > 0) {
+		} else if (cname.equals("resolve") && (args.length > 0)) {
 			String id = args[0];
 			if (rm.getReport(id) == null) {
-				// sender.sendMessage(ChatColor.RED + "[bR] No report with that id, use '/read' to see all unresolved
+				// sender.sendMessage(ChatColor.RED + "[bR] No report with that id, use '/read'
+				// to see all unresolved
 				// reports.");
 				mm.msg(sender, Msg.UNSOLVED_REPORTS, "read");
 				return true;
@@ -179,13 +194,15 @@ public class Commands implements CommandExecutor {
 					return true;
 				}
 				report.setResolved(true);
-				// sender.sendMessage(ChatColor.AQUA + "[bR] " + ChatColor.GRAY + "Report resolved.");
+				// sender.sendMessage(ChatColor.AQUA + "[bR] " + ChatColor.GRAY + "Report
+				// resolved.");
 				mm.msg(sender, Msg.REPORT_IS_RESOLVED);
 				// Inform the player that their report has been resolved if they are online
 				if (rp.getServer().getPlayerExact(report.getReporter()) != null) {
 					@SuppressWarnings("unused")
 					Player player = rp.getServer().getPlayerExact(report.getReporter());
-					// player.sendMessage(ChatColor.AQUA + "[bR] " + ChatColor.GRAY + "A report you filed has been
+					// player.sendMessage(ChatColor.AQUA + "[bR] " + ChatColor.GRAY + "A report you
+					// filed has been
 					// resolved by " + ChatColor.AQUA + rp.getName(sender));
 					mm.msg(sender, Msg.REPORT_RESOLVED, rp.getName(sender));
 					// player.sendMessage(ChatColor.GRAY + "** " + report.getReport());
@@ -193,10 +210,11 @@ public class Commands implements CommandExecutor {
 				}
 				return true;
 			}
-		} else if (cname.equals("unresolve") && args.length > 0) {
+		} else if (cname.equals("unresolve") && (args.length > 0)) {
 			String id = args[0];
 			if (rm.getReport(id) == null) {
-				// sender.sendMessage(ChatColor.RED + "[bR] No report with that id, use '/read' to see all unresolved
+				// sender.sendMessage(ChatColor.RED + "[bR] No report with that id, use '/read'
+				// to see all unresolved
 				// reports.");
 				mm.msg(sender, Msg.UNSOLVED_REPORTS, "read");
 				return true;
@@ -208,21 +226,24 @@ public class Commands implements CommandExecutor {
 					return true;
 				}
 				report.setResolved(false);
-				// sender.sendMessage(ChatColor.AQUA + "[bR] " + ChatColor.GRAY + "Report unresolved.");
+				// sender.sendMessage(ChatColor.AQUA + "[bR] " + ChatColor.GRAY + "Report
+				// unresolved.");
 				mm.msg(sender, Msg.REPORT_UNRESOLVED);
 				return true;
 			}
-		} else if (cname.equals("gotoreport") && args.length > 0) {
+		} else if (cname.equals("gotoreport") && (args.length > 0)) {
 			// Again, only players can do this one
 			if (!(sender instanceof Player)) {
-				// sender.sendMessage(ChatColor.RED + "[bR] Only players can use the '/gotoreport' command!");
+				// sender.sendMessage(ChatColor.RED + "[bR] Only players can use the
+				// '/gotoreport' command!");
 				mm.msg(sender, Msg.INGAMECOMMAND, "/gotoreport");
 				return true;
 			}
 			String id = args[0];
 			if (rm.getReport(id) == null) {
 				mm.msg(sender, Msg.UNSOLVED_REPORTS, "read");
-				// sender.sendMessage(ChatColor.RED + "[bR] No report with that id, use '/read' to see all unresolved
+				// sender.sendMessage(ChatColor.RED + "[bR] No report with that id, use '/read'
+				// to see all unresolved
 				// reports.");
 				return true;
 			} else {
@@ -232,7 +253,8 @@ public class Commands implements CommandExecutor {
 				player.teleport(loc);
 				player.getLocation().setYaw(r.getYaw());
 				player.getLocation().setPitch(r.getPitch());
-				// sender.sendMessage(ChatColor.AQUA + "[bR] " + ChatColor.GRAY + "Teleported to the location of the
+				// sender.sendMessage(ChatColor.AQUA + "[bR] " + ChatColor.GRAY + "Teleported to
+				// the location of the
 				// report - ID: " + ChatColor.AQUA + r.getID());
 				mm.msg(sender, Msg.REPORT_TELEPORT, r.getID());
 				return true;
@@ -243,7 +265,8 @@ public class Commands implements CommandExecutor {
 				report = rm.getReport(args[0]);
 				if (report == null) {
 					mm.msg(sender, Msg.UNSOLVED_REPORTS, "read");
-					// sender.sendMessage(ChatColor.RED + "[bR] No report with that id, use '/read' to see all
+					// sender.sendMessage(ChatColor.RED + "[bR] No report with that id, use '/read'
+					// to see all
 					// unresolved reports.");
 					return true;
 				}
@@ -266,9 +289,9 @@ public class Commands implements CommandExecutor {
 				}
 			}
 			return true;
-		} else if (cname.equals("modchat") && args.length > 0) {
+		} else if (cname.equals("modchat") && (args.length > 0)) {
 			String name = rp.getName(sender);
-			
+
 			StringBuilder sb = new StringBuilder();
 			for (String arg : args) {
 				sb.append(arg).append(" ");
@@ -277,18 +300,34 @@ public class Commands implements CommandExecutor {
 			// Send the message to online mods/admins
 			for (Player player : rp.getServer().getOnlinePlayers()) {
 				if (player.hasPermission("breport.modchat")) {
-					player.sendMessage(
-							ChatColor.AQUA + mm.getName() + ChatColor.GRAY + name + ChatColor.AQUA + ": " + message);
+					player.sendMessage(ChatColor.AQUA + mm.getName() + ChatColor.GRAY + name + ChatColor.AQUA + ": " + message);
 				}
 			}
 			// Also log it to the console
 			rp.log(name + ": " + message);
 			return true;
+		} else if (cname.equalsIgnoreCase("rgui")) {
+			if (!(sender instanceof Player)) {
+				sender.sendMessage(ChatColor.RED + "You must be a player!");
+				return true;
+			}
+			Player player = (Player) sender;
+			if (!player.hasPermission("breport.gui")) {
+				player.sendMessage(ChatColor.RED + "You don't have permission for that!");
+				return true;
+			}
+			// Open the GUI
+
+			// Update the main menu
+			GUI.createMainMenu(rp, player);
+			// Show it to the player
+			player.openInventory(GUI.main);
+			return true;
 		}
-		
+
 		return false;
 	}
-	
+
 	/**
 	 * @return the rp
 	 */
@@ -296,4 +335,3 @@ public class Commands implements CommandExecutor {
 		return rp;
 	}
 }
-
